7667c00a1e514f73d6c83889b8d817fc55317cfb
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2021-06-02 15:55:49 -0300

    Implementado teste de parâmetros de spam

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -20,7 +20,7 @@ from libpythonpro.spam.modelos import Usuario
 def test_qde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'matheuscbsmsn1994@gmail.com',
@@ -28,3 +28,32 @@ def test_qde_de_spam(sessao, usuarios):
         'Confira os módulos fantásticos'
     )
     assert len(usuarios) == enviador.qtd_email_enviados
+
+
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Matheus', email='matheuscbsmsn1994@gmail.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'bahsantos1996@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
+    assert enviador.parametros_de_envio == (
+        'bahsantos1996@gmail.com',
+        'matheuscbsmsn1994@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
